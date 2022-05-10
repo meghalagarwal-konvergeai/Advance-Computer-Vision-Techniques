4e68ccda8685ceee703d3cfed0fb79be56a6608c
meghalagarwal-konvergeai <meghal.agarwal@konverge.ai>
2022-05-10 16:42:35 +0530

    Advance Computer Vision Tehniques

---
+++ AI Virtual Painter/AIVirtualPainterProject.py
@@ -0,0 +1,107 @@
+import cv2 as cv
+import numpy as np
+import time
+import os
+import HandTrackingModule as htm
+
+brushThickness = 25
+eraserThickness = 100
+
+folderPath = "/home/meghal/Personal/Konverge_AI/Training/Advance Computer Visions/AI Virtual Painter/Header"
+myList = os.listdir(folderPath)
+myList.sort()
+print(myList)
+overlayList = []
+
+for imPath in myList:
+    image = cv.imread(f"{folderPath}/{imPath}")
+    overlayList.append(image)
+    print(len(overlayList))
+    header = overlayList[0]
+    drawColor = (255, 0, 255)
+
+    cap = cv.VideoCapture(0)
+    cap.set(3, 1280)
+    cap.set(4, 720)
+
+    detector = htm.handDetector(detectionCon=0,maxHands=1)
+    xp, yp = 0, 0
+    vidCanvas = np.zeros((720, 1280, 3), np.uint8)
+
+    while True:
+
+        # 1. Import image
+        success, vid = cap.read()
+        vid = cv.flip(vid, 1)
+
+        # 2. Find Hand Landmarks
+        vid = detector.findHands(vid)
+        lmList, bbox = detector.findPosition(vid, draw=False)
+        print(lmList)
+        if len(lmList) != 0:
+            
+            # tip of index and middle fingers
+            x1, y1 = lmList[8][1:]
+            x2, y2 = lmList[12][1:]
+
+        # 3. Check which fingers are up
+        fingers = detector.fingersUp()
+        # print(fingers)
+
+        # 4. If Selection Mode – Two finger are up
+        if fingers[1] and fingers[2]:
+            # xp, yp = 0, 0
+            print("Selection Mode")
+
+        # # Checking for the click
+        if y1 < 125:
+            if 250 < x1 < 450:
+                header = overlayList[0]
+                drawColor = (255, 0, 255)
+            elif 550 < x1 < 750:
+                header = overlayList[1]
+                drawColor = (255, 0, 0)
+            elif 800 < x1 < 950:
+                header = overlayList[2]
+                drawColor = (0, 255, 0)
+            elif 1050 < x1 < 1200:
+                header = overlayList[3]
+                drawColor = (0, 0, 0)
+            cv.rectangle(vid, (x1, y1 - 25), (x2, y2 + 25), drawColor, cv.FILLED)
+
+        # 5. If Drawing Mode – Index finger is up
+        if fingers[1] and fingers[2] == False:
+            cv.circle(vid, (x1, y1), 15, drawColor, cv.FILLED)
+            print("Drawing Mode")
+        if xp == 0 and yp == 0:
+            xp, yp = x1, y1
+
+        cv.line(vid, (xp, yp), (x1, y1), drawColor, brushThickness)
+
+        # if drawColor == (0, 0, 0):
+        # cv.line(vid, (xp, yp), (x1, y1), drawColor, eraserThickness)
+        # cv.line(vidCanvas, (xp, yp), (x1, y1), drawColor, eraserThickness)
+        #
+        # else:
+        # cv.line(vid, (xp, yp), (x1, y1), drawColor, brushThickness)
+        # cv.line(vidCanvas, (xp, yp), (x1, y1), drawColor, brushThickness)
+
+        xp, yp = x1, y1
+
+        # # Clear Canvas when all fingers are up
+        # if all (x >= 1 for x in fingers):
+        # vidCanvas = np.zeros((720, 1280, 3), np.uint8)
+
+        vidGray = cv.cvtColor(vidCanvas, cv.COLOR_BGR2GRAY)
+        _, vidInv = cv.threshold(vidGray, 50, 255, cv.THRESH_BINARY_INV)
+        vidInv = cv.cvtColor(vidInv,cv.COLOR_GRAY2BGR)
+        vid = cv.bitwise_and(vid,vidInv)
+        vid = cv.bitwise_or(vid,vidCanvas)
+
+        # Setting the header image
+        vid[0:125, 0:1280] = header
+        # vid = cv.addWeighted(vid,0.5,vidCanvas,0.5,0)
+        cv.imshow("Video", vid)
+        cv.imshow("Video Canvas", vidCanvas)
+        cv.imshow("Video Inv", vidInv)
+        cv.waitKey(1)
---
+++ AI Virtual Painter/HandTrackingModule.py
@@ -0,0 +1,115 @@
+import cv2 as cv
+import mediapipe as mp
+import time
+import math
+import numpy as np
+ 
+class handDetector():
+    def __init__(self, mode=False, maxHands=2, detectionCon=0, trackCon=0):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+ 
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,
+        self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+        self.tipIds = [4, 8, 12, 16, 20]
+ 
+    def findHands(self, vid, draw=True):
+        vidRGB = cv.cvtColor(vid, cv.COLOR_BGR2RGB)
+        self.results = self.hands.process(vidRGB)
+        # print(results.multi_hand_landmarks)
+    
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(vid, handLms,
+                    self.mpHands.HAND_CONNECTIONS)
+    
+        return vid
+    
+    def findPosition(self, vid, handNo=0, draw=True):
+        xList = []
+        yList = []
+        bbox = []
+        self.lmList = []
+
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                # print(id, lm)
+                h, w, c = vid.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                xList.append(cx)
+                yList.append(cy)
+                #print(id, cx, cy)
+                self.lmList.append([id, cx, cy])
+                if draw:
+                    cv.circle(vid, (cx, cy), 5, (255, 0, 255), cv.FILLED)
+                
+        xmin, xmax = min(xList), max(xList)
+        ymin, ymax = min(yList), max(yList)
+        bbox = xmin, ymin, xmax, ymax                
+        
+        if draw:
+            cv.rectangle(vid, (xmin-20, ymin-20), (xmax+20, ymax+20), (0, 255, 0), 2)
+    
+        return self.lmList , bbox
+    
+    def fingersUp(self):
+        fingers = []
+        print(f"lmList Details = {self.lmList[self.tipIds[0]][1]}")
+        # Thumb
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+    
+        # Fingers
+        for id in range(1, 5):
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+    
+            # totalFingers = fingers.count(1)
+    
+        return fingers
+    
+    def findDistance(self, p1, p2, vid, draw=True,r=15, t=3):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+    
+        if draw:
+            cv.line(vid, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv.circle(vid, (x1, y1), r, (255, 0, 255), cv.FILLED)
+            cv.circle(vid, (x2, y2), r, (255, 0, 255), cv.FILLED)
+            cv.circle(vid, (cx, cy), r, (0, 0, 255), cv.FILLED)
+            length = math.hypot(x2 - x1, y2 - y1)
+    
+        return length, vid, [x1, y1, x2, y2, cx, cy]
+    
+if __name__ == "__main__":
+    pTime = 0
+    cTime = 0
+    cap = cv.VideoCapture(0)
+    detector = handDetector()
+    while True:
+        success, vid = cap.read()
+        vid = detector.findHands(vid)
+        lmList, bbox = detector.findPosition(vid)
+        if len(lmList) != 0:
+            print(lmList)
+ 
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+ 
+        cv.putText(vid, str(int(fps)), (10, 70), cv.FONT_HERSHEY_PLAIN, 3,
+        (255, 0, 255), 3)
+ 
+        cv.imshow("Video", vid)
+        cv.waitKey(1)
